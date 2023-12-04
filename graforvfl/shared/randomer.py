59e63c2c41bf7c6d26cac693470d15cb87e89f43
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-12-04 10:07:00 +0700

    Add randomer module

---
+++ graforvfl/shared/randomer.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 02:44, 04/12/2023 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import numpy as np
+
+
+def get_correct_shape(shape):
+    if type(shape) is int:
+        return (shape, 1)
+    elif type(shape) in (list, tuple, np.ndarray) and len(shape) == 1:
+        return (shape, 1)
+    else:
+        return shape
+
+
+def orthogonal_initializer(shape, gain=1.0):
+    shape = get_correct_shape(shape)
+    flat_shape = (shape[0], np.prod(shape[1:]))
+    a = np.random.normal(0.0, 1.0, flat_shape)
+    u, _, v = np.linalg.svd(a, full_matrices=False)
+    q = u if u.shape == flat_shape else v
+    q = q.reshape(shape)
+    return gain * q[:shape[0], :shape[1]]
+
+
+def he_uniform_initializer(shape):
+    shape = get_correct_shape(shape)
+    fan_in = shape[0] if len(shape) == 2 else np.prod(shape[1:])
+    limit = np.sqrt(6 / fan_in)
+    return np.random.uniform(-limit, limit, shape)
+
+
+def he_normal_initializer(shape):
+    shape = get_correct_shape(shape)
+    fan_in = shape[0] if len(shape) == 2 else np.prod(shape[1:])
+    stddev = np.sqrt(2 / fan_in)
+    return np.random.normal(0.0, stddev, shape)
+
+
+def glorot_uniform_initializer(shape):
+    shape = get_correct_shape(shape)
+    fan_in, fan_out = shape
+    limit = np.sqrt(6 / (fan_in + fan_out))
+    return np.random.uniform(-limit, limit, shape)
+
+
+def glorot_normal_initializer(shape):
+    shape = get_correct_shape(shape)
+    fan_in, fan_out = shape
+    stddev = np.sqrt(2 / (fan_in + fan_out))
+    return np.random.normal(0.0, stddev, shape)
+
+
+def lecun_uniform_initializer(shape):
+    shape = get_correct_shape(shape)
+    fan_in = shape[0] if len(shape) == 2 else np.prod(shape[1:])
+    limit = np.sqrt(3 / fan_in)
+    return np.random.uniform(-limit, limit, shape)
+
+
+def lecun_normal_initializer(shape):
+    shape = get_correct_shape(shape)
+    fan_in = shape[0] if len(shape) == 2 else np.prod(shape[1:])
+    stddev = np.sqrt(1 / fan_in)
+    return np.random.normal(0.0, stddev, shape)
+
+
+def random_uniform_initializer(shape, minval=0.0, maxval=1.0):
+    return np.random.uniform(minval, maxval, shape)
+
+
+def random_normal_initializer(shape, mean=0.0, stddev=1.0):
+    return np.random.normal(mean, stddev, shape)
