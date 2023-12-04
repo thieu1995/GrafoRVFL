dff26e5e728ea8c25b76be6ec04e6490556195e0
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-12-04 10:23:29 +0700

    Update init

---
+++ graforvfl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 15:23, 10/08/2023 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+__version__ = "1.0.0"
+
+from graforvfl.shared.data_processor import DataTransformer, Data, get_dataset
+from graforvfl.network.standard_rvfl import RvflRegressor, RvflClassifier
