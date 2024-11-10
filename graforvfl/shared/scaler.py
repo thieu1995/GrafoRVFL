0f15113cec6ab73015d3ee2fb15db69cc1f143f8
anh9895 <honganh090895@gmail.com>
2024-11-10 23:25:00 +0700

    Fix bug OneHotEncoder

--- graforvfl/shared/scaler.py
+++ graforvfl/shared/scaler.py
@@ -8,7 +8,41 @@ import numpy as np
 from scipy.stats import boxcox, yeojohnson
 from scipy.special import inv_boxcox
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler
+
+
+class OneHotEncoder:
+    def __init__(self):
+        self.categories_ = None
+
+    def fit(self, X):
+        """Fit the encoder to unique categories in X."""
+        self.categories_ = np.unique(X)
+        return self
+
+    def transform(self, X):
+        """Transform X into one-hot encoded format."""
+        if self.categories_ is None:
+            raise ValueError("The encoder has not been fitted yet.")
+        one_hot = np.zeros((X.shape[0], len(self.categories_)), dtype=int)
+        for i, val in enumerate(X):
+            index = np.where(self.categories_ == val)[0][0]
+            one_hot[i, index] = 1
+        return one_hot
+
+    def fit_transform(self, X):
+        """Fit the encoder to X and transform X."""
+        self.fit(X)
+        return self.transform(X)
+
+    def inverse_transform(self, one_hot):
+        """Convert one-hot encoded format back to original categories."""
+        if self.categories_ is None:
+            raise ValueError("The encoder has not been fitted yet.")
+        if one_hot.shape[1] != len(self.categories_):
+            raise ValueError("The shape of the input does not match the number of categories.")
+        original = np.array([self.categories_[np.argmax(row)] for row in one_hot])
+        return original
 
 
 class LabelEncoder:
@@ -104,7 +138,7 @@ class ObjectiveScaler:
             data[np.where(data == 0)] = -1
             return data
         elif self.obj_name == "softmax":
-            data = self.ohe_scaler.transform(np.reshape(data, (-1, 1)))
+            data = self.ohe_scaler.fit_transform(np.reshape(data, (-1, 1)))
             return data
 
     def inverse_transform(self, data):
